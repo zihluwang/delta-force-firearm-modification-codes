9dfd52eb2dfe8cfce02517c6afebcde4f5dc12fc
zihluwang <real@zihluwang.me>
2026-04-07 11:06:34 +0800

    feat: integrate Ant Design components and configuration for improved UI

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from "react-dom/client"
 import { RouterProvider } from "react-router-dom"
 import { Provider } from "react-redux"
 import { PersistGate } from "redux-persist/integration/react"
+import { App as AntApp, ConfigProvider as AntConfigProvider } from "antd"
+import { StyleProvider as AntStyleProvider } from "@ant-design/cssinjs"
+import AntSimplifiedChinese from "antd/locale/zh_CN"
 import "@/init"
 import router from "@/router"
 import store, { persistor } from "@/store"
@@ -16,8 +19,18 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <RouterProvider router={router} />
+        <AntStyleProvider layer>
+          <AntConfigProvider
+            locale={AntSimplifiedChinese}
+            button={{
+              autoInsertSpace: false,
+            }}>
+            <AntApp className="h-full w-full">
+              <RouterProvider router={router} />
+            </AntApp>
+          </AntConfigProvider>
+        </AntStyleProvider>
       </PersistGate>
     </Provider>
-  </StrictMode>,
+  </StrictMode>
 )
