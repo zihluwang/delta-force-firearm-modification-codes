d663cc5d200671e3686ab0280cad8b3cfea8fbe6
zihluwang <real@zihluwang.me>
2026-05-06 19:01:58 +0800

    docs: add js docs

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,14 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
+  /**
+   * Redux persistent storage location, use `local` for local storage and `session` for
+   * session storage
+   */
   readonly VITE_REDUX_STORAGE: "local" | "session"
+
+  /**
+   * Backend API Base URL
+   */
   readonly VITE_API_BASE_URL: string
 }
 
